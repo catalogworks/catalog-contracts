d7ec935a9582d966d8854da90902a0254c815b8c
brett <bretth18@gmail.com>
2021-12-04 22:38:58 -0800

    procrastinating packing by making this really good tree script

--- scripts/tree.ts
+++ scripts/tree.ts
@@ -2,16 +2,56 @@
 // utility to generate merkle tree
 // prints root and proof 
 // can use to input on etherscan or other tools
-
-import{ ethers} from 'hardhat'
+import hre from 'hardhat';
 import  keccak256  from 'keccak256';
 import { MerkleTree } from 'merkletreejs';
 
-async function main() {
 
+async function getInput(): Promise<string> {
+    const readline = require('readline');
+
+    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+    const prompt = (query: any) => new Promise<string>((resolve) => rl.question(query, resolve));
+    //   try{
+    //     const name = await prompt('Input Address: ')
+    //     //can use name for next question if needed
+    //     //can prompt multiple times.
+    //     console.log(name);
+    //     address = String(name);
+    //     rl.close()
+    //   }catch(e){
+    //     console.error("unable to prompt",e)
+    // }
+    
+    const address = await prompt('\x1b[33m Input Address (to add to tree, and generate proof): \x1b[0m');
+       
+    //when done reading prompt exit program 
+    rl.on('close', () => {
+        return address;
+        process.exit(0)
+    });
+
+    return address;
+
+}
+async function main(inputAddress?: string) {
+
+        console.group(
+           '\n \n \x1b[34m','༼ つ ◕_◕ ༽つ the tree utility ༼ つ ◕_◕ ༽つ', '\x1b[0m \n \n',
+        );
+
+
+        let address: string = inputAddress || '0x8a5847fd0e592B058c026C5fDc322AEE834B87F5';
+
+        if (inputAddress) {
+            console.log('with input address:', inputAddress);
+            address = inputAddress;
+        }
+
+        
         // PUT WHATEVER ADDRESS YOU WANNA INPUT HERE, PASSING ARGS DOESN"T WORK FOR SOME REASON
         // AHHHHHH I"M MERKLE TREEEEING
-        const address = '0x8a5847fd0e592B058c026C5fDc322AEE834B87F5';
+            
 
         // make a tree 
         const leaves = [
@@ -24,21 +64,22 @@ async function main() {
             '0x37b95b2fe82b35b01e475d3578e19614b2a8bef7',
             '0x8d165b4ca6055a9a41b1fe50d1ebaab2efe44385',
             '0x2ed0db8d2870ccac48b1693b9efe4341fedaecb1',
+            inputAddress,
             // address?.toString(),           
         ].map((x) => keccak256(x));
 
         const tree = new MerkleTree(leaves, keccak256, { sortPairs: true});
         const root = tree.getHexRoot();
 
-        console.log('tree:', tree);
+        console.log('tree:', tree.toString());
         console.log('\x1b[36m%s\x1b[0m','root (what you send):', root);
 
-
+        
         const leaf = await keccak256(address);
 
         const proof =  tree.getHexProof(leaf);
-        console.log('\x1b[33m%s\x1b[0m','proof for address: ');
-        console.log('\x1b[36m%s\x1b[0m', `${address}`, '', proof.toString());
+        console.log('\x1b[33m%s\x1b[0m','proof for address:  (copy the red)');
+        console.log('\x1b[36m%s\x1b[0m', `${address}`, '\n \n', '\x1b[31m',  proof.toString(), '\x1b[0m');
 
 
         return {root, proof};
@@ -46,10 +87,18 @@ async function main() {
 }
 
 
+getInput()
+.then((val) => {
+    main(val).then(() => process.exit(0));
+})
+.catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
 
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+// main()
+//     .then(() => process.exit(0))
+//     .catch((error) => {
+//         console.error(error);
+//         process.exit(1);
+//     });
