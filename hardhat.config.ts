c0bfc4a0962843d18f663af888213da39fb2f0c1
brett <bretth18@gmail.com>
2021-12-16 15:40:02 -0800

    fix config for forking

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -43,6 +43,8 @@ if (process.env.DEPLOYER) {
   console.log('\x1b[34m','DEPLOYER flag is set to true, using private keys from .env...','\x1b[0m');
 }
 
+const forkBool = (process.env.HARDHAT_DEPLOY_FORK ? true : false);
+
 
 // SET 'process.env.DEPLOYER = TRUE' TO USE PRIVATE KEY ACCOUNTS STORED IN .env
 // DEFAULTS TO USE DUMMY MNEMONIC ACCOUNTS FOR EASE AND TESTING
@@ -95,7 +97,7 @@ const config: HardhatUserConfig = {
       forking: {
         url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
         // DISABLE IF NO ALCHEMY
-        enabled: true,
+        enabled: forkBool,
         blockNumber: 13674612,
       },
 
