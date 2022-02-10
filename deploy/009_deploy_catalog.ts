c3ca7e511f9033e59d0938fc1a35a5a82fd9330a
Brett Henderson <bretth18@gmail.com>
2022-02-10 12:48:25 -0800

    succesfully upgraded rinkeby proxy to new implementation

--- deploy/009_deploy_catalog.ts
+++ deploy/009_deploy_catalog.ts
@@ -30,24 +30,26 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         autoMine: true, // speeds deployment on local network. no effect on testnet/mainnet
     });
 
-    const deployCatalogUUPSUpgrade = await catchUnknownSigner(
-        deploy('Catalog', {
-            contract: 'Catalog',
-            from: multisig,
-            args: [],
-            proxy: {
-                proxyContract: 'ERC1967Proxy',
-                proxyArgs: ['{implementation}', '{data}'],
-                execute: {
-                    init: {
-                        methodName: 'initialize',
-                        args: ['Catalog', 'CNFT'],
-                    },
-                },
-            },
-        })
-    );
-    
+    // const deployCatalogUUPSUpgrade = await catchUnknownSigner(
+    //     deploy('Catalog', {
+    //         contract: 'Catalog',
+    //         from: multisig,
+    //         args: [],
+    //         proxy: {
+    //             proxyContract: 'ERC1967Proxy',
+    //             proxyArgs: ['{implementation}', '{data}'],
+    //             execute: {
+    //                 init: {
+    //                     methodName: 'initialize',
+    //                     args: ['Catalog', 'CNFT'],
+    //                 },
+    //             },
+    //         },
+    //         log: true,
+    //     })
+    // );
+
+    // console.log(deployCatalogUUPSUpgrade);
 
     if (deployCatalogUUPS && deployCatalogUUPS.newlyDeployed) {
         log(
